fd05f5814a83d4805c1be9df1c13d65b2f96c05b
Denny <dennythecoder@gmail.com>
2017-10-17 10:35:46 -0500

    clean toc

--- src/components/TOC.vue
+++ src/components/TOC.vue
@@ -23,23 +23,15 @@
 	}
 </style>
 <script>
-mb.store = mb.store || {};
-
-module.exports = {
+export default {
   computed: {
     chapters: function() {
-      if (!mb.store.chapters) {
-        Vue.set(mb.store, "chapters", []);
-        return [];
-      }
-      return mb.store.chapters;
+      return this.$store.getters.chapters;
     }
   },
   methods: {
     gotoChapter: function(chapter) {
-      let vm = this;
       const cfi = chapter.cfi.replace(/\//g, "-"); // making url friendly
-      // vm.$router.push({path:'reader/'+cfi});
       window.location.hash = "#/reader/" + cfi;
     }
   }
