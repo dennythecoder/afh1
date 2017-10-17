7e1591eb33802141c337d4e35d8a4f9f25f1323e
Denny <dennythecoder@gmail.com>
2017-10-17 08:27:12 -0500

    switching to vuex

--- src/store.js
+++ src/store.js
@@ -1,24 +1,26 @@
 import Vue from "vue";
 import router from "./router";
+import Vuex from "vuex";
 
-let store = new Vue({
-  data: {
+
+const store =  new Vuex.Store({
+  state:{
     bookmarks: [],
     lastLocation: {},
     book: {},
     chapters: [],
     isBookInitialized: false,
-    pages: []
+    pages: [],
+    router
   },
-  router,
-  computed: {
-    isReader() {
-      return this.$route.path.indexOf("/reader") !== -1;
+  getters: {
+    isReader(state) {
+      return state.$route.path.indexOf("/reader") !== -1;
     },
-    isSearcher() {
-      return this.$route.path.indexOf("/searcher") !== -1;
+    isSearcher(state) {
+      return state.$route.path.indexOf("/searcher") !== -1;
     },
-    isBookmarked() {
+    isBookmarked(state) {
       let bookmarks = this.bookmarks,
         lastLocation = this.lastLocation;
       for (var i = 0; i < bookmarks.length; i++) {
@@ -29,8 +31,8 @@ let store = new Vue({
       return false;
     }
   },
-  methods: {
-    generatePagination() {
+  mutation: {
+    generatePagination(state) {
       let store = this;
       store.pages.splice(1, this.pages.length - 1);
       const localStoragePages = localStorage.getItem("pages");
@@ -60,82 +62,84 @@ let store = new Vue({
         }
       }
       return result;
-    },
-
-    setBook(book) {
-      Vue.set(this, "book", book);
-      this.isBookInitialized = true;
-    },
-
-    createBookmark() {
-      this.saveLastLocation();
-      var bookmark = this.lastLocation;
-      this.bookmarks.push(bookmark);
-      localStorage.setItem("bookmarks", JSON.stringify(this.bookmarks));
-    },
-    destroyBookmark() {
-      for (var i = 0; i < this.bookmarks.length; i++) {
-        const bookmark = this.bookmarks[i];
-        if (bookmark.location === this.lastLocation.location) {
-          this.bookmarks.splice(i, 1);
-          localStorage.setItem("bookmarks", JSON.stringify(this.bookmarks));
-        }
-      }
-    },
-    gotoCfi(cfi) {
-      // expecting string like this -- epubcfi(/6/2[titlepage]!/4/1:0)
-      let store = this;
-      store.book.gotoCfi(cfi).then(function() {
-        store.$forceUpdate();
-        store.saveLastLocation();
-      });
-    },
-    nextPage() {
-      this.book.nextPage();
-      this.saveLastLocation();
-    },
-    prevPage() {
-      this.book.prevPage();
-      this.saveLastLocation();
-    },
-
-    saveLastLocation() {
-      let store = this;
-      function getChapterName(href) {
-        for (var i = 0; i < store.chapters.length; i++) {
-          if (store.chapters[i].href === href) {
-            return store.chapters[i].label;
-          }
-        }
-      }
-
-      const cfi = this.book.getCurrentLocationCfi();
-      let result = /epubcfi\((.*)\)/.exec(cfi);
-      let location = result[1].replace(/\//g, "-");
-      let href = this.book.currentChapter.href;
-      let chapterName = getChapterName(href);
-      let lastLocation = {
-        location: location,
-        href: href,
-        chapterName: chapterName
-      };
-
-      const json = JSON.stringify(lastLocation);
-
-      localStorage.setItem("lastLocation", json);
-      Vue.set(this, "lastLocation", lastLocation);
     }
   },
-  created() {
-    const jsonBookmarks = localStorage.getItem("bookmarks");
-    if (jsonBookmarks) {
-      Vue.set(this, "bookmarks", JSON.parse(jsonBookmarks));
+
+  setBook(state, book) {
+    state.book = book;
+    state.isBookInitialized = true;
+  },
+
+  createBookmark() {
+    this.saveLastLocation();
+    var bookmark = this.lastLocation;
+    this.bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(this.bookmarks));
+  },
+  destroyBookmark() {
+    for (var i = 0; i < this.bookmarks.length; i++) {
+      const bookmark = this.bookmarks[i];
+      if (bookmark.location === this.lastLocation.location) {
+        this.bookmarks.splice(i, 1);
+        localStorage.setItem("bookmarks", JSON.stringify(this.bookmarks));
+      }
     }
-    const jsonLastLocation = localStorage.getItem("lastLocation");
-    if (jsonLastLocation) {
-      Vue.set(this, "lastLocation", JSON.parse(jsonLastLocation));
+  },
+  gotoCfi(cfi) {
+    // expecting string like this -- epubcfi(/6/2[titlepage]!/4/1:0)
+    let store = this;
+    store.book.gotoCfi(cfi).then(function() {
+      store.$forceUpdate();
+      store.saveLastLocation();
+    });
+  },
+  nextPage() {
+    this.book.nextPage();
+    this.saveLastLocation();
+  },
+  prevPage() {
+    this.book.prevPage();
+    this.saveLastLocation();
+  },
+
+  saveLastLocation() {
+    let store = this;
+    function getChapterName(href) {
+      for (var i = 0; i < store.chapters.length; i++) {
+        if (store.chapters[i].href === href) {
+          return store.chapters[i].label;
+        }
+      }
     }
+
+    const cfi = this.book.getCurrentLocationCfi();
+    let result = /epubcfi\((.*)\)/.exec(cfi);
+    let location = result[1].replace(/\//g, "-");
+    let href = this.book.currentChapter.href;
+    let chapterName = getChapterName(href);
+    let lastLocation = {
+      location: location,
+      href: href,
+      chapterName: chapterName
+    };
+
+    const json = JSON.stringify(lastLocation);
+
+    localStorage.setItem("lastLocation", json);
+    Vue.set(this, "lastLocation", lastLocation);
   }
 });
 
-export default store;
+function init(store) {
+  const jsonBookmarks = localStorage.getItem("bookmarks");
+  if (jsonBookmarks) {
+    Vue.set(store, "bookmarks", JSON.parse(jsonBookmarks));
+  }
+  const jsonLastLocation = localStorage.getItem("lastLocation");
+  if (jsonLastLocation) {
+    Vue.set(store, "lastLocation", JSON.parse(jsonLastLocation));
+  }
+  return store;
+}
+
+export default init(store)
